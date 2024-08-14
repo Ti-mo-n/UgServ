f7ddfa5d61880355199c224ded2f9ebaf02c4426
Ti-mo-n <timonnuwagaba@gmail.com>
2024-08-15 02:25:48 +0300

    email

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react' 
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser'
+import { defaultStyles } from '@/constants/Styles';
+import Colors from '@/constants/Colors';
 
 const Page = () => {
+    useWarmUpBrowser();
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+           <TextInput autoCapitalize='none' placeholder='Email' style={[defaultStyles.inputField, {marginBottom: 30}]}/>
+           <TouchableOpacity style={defaultStyles.btn}>
+            <Text style={defaultStyles.btnText}>Continue</Text>
+           </TouchableOpacity>
+           <View style={styles.seperatorView}>
+            <View style={{
+                flex: 1,
+                borderBlockColor: '#000',
+                borderBottomWidth: StyleSheet.hairlineWidth,
+            }} />
+            <Text style={styles.seperator}>or</Text>
+            <View style={{
+                flex: 1,
+                borderBlockColor: '#000',
+                borderBottomWidth: StyleSheet.hairlineWidth,
+            }} />
+           </View>
+           <View></View>
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 26
+    },
+    seperatorView: {
+        flexDirection: 'row',
+        gap: 10,
+        alignItems: 'center',
+        marginVertical: 30,
+    },
+    seperator: {
+        fontFamily: 'mon-sb',
+        color: Colors.grey,
+    }
+})
 export default Page
