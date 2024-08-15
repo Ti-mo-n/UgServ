5f512f00caf90f3cb7d5b69f03b50b28fe4443b7
Ti-mo-n <timonnuwagaba@gmail.com>
2024-08-15 12:09:11 +0300

    buttons

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser'
 import { defaultStyles } from '@/constants/Styles';
 import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 const Page = () => {
     useWarmUpBrowser();
@@ -25,7 +26,24 @@ const Page = () => {
                 borderBottomWidth: StyleSheet.hairlineWidth,
             }} />
            </View>
-           <View></View>
+           <View style={{gap: 20}}>
+            <TouchableOpacity style={styles.btnOutline}>
+                <Ionicons name='call-outline' size={22} style={defaultStyles.btnIcon} />
+                <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btnOutline}>
+                <Ionicons name='logo-apple' size={24} style={defaultStyles.btnIcon} />
+                <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btnOutline}>
+                <Ionicons name='logo-google' size={24} style={defaultStyles.btnIcon} />
+                <Text style={styles.btnOutlineText}>Continue with Google</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btnOutline}>
+                <Ionicons name='logo-facebook' size={24} style={defaultStyles.btnIcon} />
+                <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+            </TouchableOpacity>
+           </View>
         </View>
     )
 }
@@ -45,6 +63,22 @@ const styles = StyleSheet.create({
     seperator: {
         fontFamily: 'mon-sb',
         color: Colors.grey,
-    }
+    },
+    btnOutline: {
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: Colors.grey,
+        height: 50,
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+    },
+    btnOutlineText: {
+        color: '#000',
+        fontSize: 16,
+        fontFamily: 'mon-sb',        
+    },
 })
 export default Page
