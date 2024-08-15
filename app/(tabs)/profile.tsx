55941b3578febb56932995b960b6488d9b2092e8
Ti-mo-n <timonnuwagaba@gmail.com>
2024-08-15 23:28:29 +0300

    profile pic

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, Button, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 import React, { useEffect, useState } from 'react' 
 import { useAuth, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router';
@@ -7,6 +7,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { defaultStyles } from '@/constants/Styles';
 import Colors from '@/constants/Colors';
 import { Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 const Page = () => {
     const { signOut, isSignedIn } = useAuth();
@@ -24,12 +25,35 @@ const Page = () => {
     }, [user]);
 
     const onSaveUser =async() => {
+        try {
+            if (!firstName || !lastName) return;
 
+            await user?.update({
+                firstName,
+                lastName
+            })
+        }catch(error) {
+            console.error(error);
+        } finally {
+            setEdit(false);
+        }        
     }
 
     const onCaptureImage =async() => {
-        
-    }
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            quality: 0.75,
+            base64: true
+        });
+
+        if (!result.canceled) {
+            const base64 = `data:image/png;base64,${result.assets[0].base64}`;
+            user?.setProfileImage({
+                file: base64,
+            })
+        }
+    };
 
     return (
         <SafeAreaView style={defaultStyles.container}>
@@ -43,6 +67,35 @@ const Page = () => {
                     <TouchableOpacity onPress={onCaptureImage}>
                         <Image source={{ uri: user?.imageUrl }} style={styles.avatar}/>
                     </TouchableOpacity>
+                    <View style={{flexDirection: 'row', gap: 6}}>
+                        {edit ? (    
+                            <View style={styles.editRow}>     
+                            <TextInput 
+                              placeholder='First name'
+                              value={firstName || ''}
+                              onChangeText={setFirstName}
+                              style={[defaultStyles.inputField, {width: 100 }]}
+                            />
+                            <TextInput 
+                              placeholder='Last name'
+                              value={lastName || ''}
+                              onChangeText={setLastName}
+                              style={[defaultStyles.inputField, {width: 100 }]}
+                            />                   
+                            <TouchableOpacity onPress={onSaveUser}>
+                                    <Ionicons name="checkmark-outline" size={24} color={Colors.dark} />
+                            </TouchableOpacity>
+                            </View>
+                        ) : (
+                            <View style={styles.editRow}>
+                                <Text style={{ fontFamily: 'mon-b', fontSize: 22 }}>{firstName} {lastName}</Text>
+                                <TouchableOpacity onPress={() => setEdit(true)}>
+                                    <Ionicons name="create-outline" size={24} color={Colors.dark} />
+                                </TouchableOpacity>
+                            </View>
+                        )}                        
+                    </View>
+                    <Text>{email}</Text>
                 </View>
             )}
 
@@ -90,6 +143,14 @@ const styles = StyleSheet.create({
         height: 100,
         borderRadius: 50,
         backgroundColor: Colors.grey,
+    },
+    editRow: {
+        height: 50,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 8,
     }
 });
 
