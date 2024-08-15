d873ee6438201b3544bdd019b7daaa8465e1cd61
Ti-mo-n <timonnuwagaba@gmail.com>
2024-08-15 12:46:35 +0300

    development

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -4,9 +4,42 @@ import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser'
 import { defaultStyles } from '@/constants/Styles';
 import Colors from '@/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
+import { useOAuth } from '@clerk/clerk-expo';
+import { useRouter } from 'expo-router';
+
+enum Strategy {
+    Google= 'oauth_google',
+    Apple= 'oauth_apple',
+    Facebook= 'oauth_facebook',
+}
 
 const Page = () => {
     useWarmUpBrowser();
+    const router = useRouter();
+    const { startOAuthFlow: googleAuth } = useOAuth({strategy: 'oauth_google'});
+    const { startOAuthFlow: appleAuth } = useOAuth({strategy: 'oauth_apple'});
+    const { startOAuthFlow: facebookAuth } = useOAuth({strategy: 'oauth_facebook'});
+
+    const onSelectAuth = async (strategy: Strategy) => {
+        const selectedAuth = {
+            [Strategy.Google]: googleAuth,
+            [Strategy.Apple]: appleAuth,
+            [Strategy.Facebook]: facebookAuth,
+        }[strategy];
+
+        try {
+            const { createdSessionId, setActive } = await selectedAuth();
+            console.log(". ~ file: login.tsx:31 ~ createdSessionId:", createdSessionId);
+
+            if (createdSessionId) {
+                setActive!({ session: createdSessionId })
+                router.back(); 
+            } 
+            }catch (err) {
+                console.error('OAuth error', err);
+        }
+    };
+
     return (
         <View style={styles.container}>
            <TextInput autoCapitalize='none' placeholder='Email' style={[defaultStyles.inputField, {marginBottom: 30}]}/>
@@ -27,20 +60,20 @@ const Page = () => {
             }} />
            </View>
            <View style={{gap: 20}}>
-            <TouchableOpacity style={styles.btnOutline}>
+            <TouchableOpacity style={styles.btnOutline} >
                 <Ionicons name='call-outline' size={22} style={defaultStyles.btnIcon} />
                 <Text style={styles.btnOutlineText}>Continue with Phone</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.btnOutline}>
+            <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Apple)}>
                 <Ionicons name='logo-apple' size={24} style={defaultStyles.btnIcon} />
                 <Text style={styles.btnOutlineText}>Continue with Apple</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.btnOutline}>
+            <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Google)}>
                 <Ionicons name='logo-google' size={24} style={defaultStyles.btnIcon} />
                 <Text style={styles.btnOutlineText}>Continue with Google</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.btnOutline}>
-                <Ionicons name='logo-facebook' size={24} style={defaultStyles.btnIcon} />
+                <Ionicons name='logo-facebook' size={24} style={defaultStyles.btnIcon} onPress={() => onSelectAuth(Strategy.Facebook)}/>
                 <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
             </TouchableOpacity>
            </View>
